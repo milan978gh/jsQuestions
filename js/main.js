a7c7cbe01d55f0171127fee0f38e4beb21db2a8d
milan978gh <amilandev78@gmail.com>
2020-11-24 18:09:24 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 // using selectors inside the element
+const questions = document.querySelectorAll('.question');
 
-
-
-// traversing the DOM
-const btns = document.querySelectorAll('.question-btn');
-
-btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
-    const question = e.currentTarget.parentElement.parentElement;
+questions.forEach(question => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    questions.forEach(item => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
     question.classList.toggle('show-text');
   });
 });
+
+
+// traversing the DOM
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     const questions = document.querySelectorAll('.question');
+//     questions.forEach(function (item) {
+//       if (item !== question) {
+//         item.classList.remove('show-text');
+//       }
+//     });
+//     question.classList.toggle('show-text');
+//   });
+// });

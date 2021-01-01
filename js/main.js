88dceb12c480336f23dfb9c9c325205f6706baba
milan978gh <amilandev78@gmail.com>
2021-01-01 13:49:28 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
-const questions = document.querySelectorAll('.question');
+// const questions = document.querySelectorAll('.question');
 
 
-questions.forEach((question) => {
-  const btn = question.querySelector('.question-btn');
+// questions.forEach((question) => {
+//   const btn = question.querySelector('.question-btn');
 
-  btn.addEventListener('click', () => {
-    questions.forEach((item) => {
-      if (item !== question) {
-        item.classList.remove('show-text');
+//   btn.addEventListener('click', () => {
+//     questions.forEach((item) => {
+//       if (item !== question) {
+//         item.classList.remove('show-text');
+//       }
+//     });
+//     question.classList.toggle('show-text');
+//   });
+// });
+
+
+const btns = document.querySelectorAll('.question-btn');
+
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const question = e.currentTarget.parentElement.parentElement;
+    const questionS = document.querySelectorAll('.question');
+
+    questionS.forEach((thing) => {
+      if (thing !== question) {
+        thing.classList.remove('show-text');
       }
     });
     question.classList.toggle('show-text');
   });
 });
+
